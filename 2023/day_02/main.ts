45d31bd1cbc78665654cc48a2c30529b7ab339bf
tonytrg <tonytrg@github.com>
2024-11-05 20:05:11 +0100

    add day 2 challenge

--- 2023/day_02/main.ts
+++ 2023/day_02/main.ts
@@ -40,19 +40,80 @@ async function part1() {
     }
     if (possible) {
       const id = line.match(/\d+/g)?.[0] ?? "0";
-      const idAsNum = parseInt(id)
+      const idAsNum = parseInt(id);
       if (!isNaN(idAsNum)) {
-        sum += idAsNum
+        sum += idAsNum;
       }
     }
   }
-  console.log("Sum of id of possible games: ", sum)
+  console.log("Sum of id of possible games: ", sum);
   rl.close();
   fileStream.close();
 }
 
 async function part2() {
-  console.log("part 2");
+  const inputPath = join(getRelativePath(import.meta.url), "input");
+  const fileStream = createReadStream(inputPath);
+  const rl = createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  const PowerOfCubes = (l: string): number => {
+    let green = 1;
+    let blue = 1;
+    let red = 1;
+
+    const throws = l.split(":")?.[1] ?? "";
+    if (throws === "") {
+      throw new Error("invalid format of line");
+    }
+    const seperated = throws?.split(";").map((s) =>
+      s
+        .split(",")
+        .map((item) => item?.trim() ?? "")
+        .filter((item) => item !== "")
+    );
+
+    for (const t of seperated) {
+      for (const tt of t) {
+        const sep = tt.split(" ");
+        const count = parseInt(sep[0]);
+        if (isNaN(count)) {
+          throw new Error("Failed to parse number");
+        }
+        switch (sep[1] ?? "") {
+          case "red":
+            if (count > red) {
+              red = count;
+            }
+            break;
+          case "blue":
+            if (count > blue) {
+              blue = count;
+            }
+            break;
+          case "green":
+            if (count > green) {
+              green = count;
+            }
+            break;
+          default:
+            throw new Error("color unknown");
+        }
+      }
+    }
+
+    return green * blue * red;
+  };
+
+  let sumPower = 0;
+
+  for await (const line of rl) {
+    sumPower += PowerOfCubes(line);
+  }
+
+  console.log("Power of sum: ", sumPower);
 }
 
 async function main() {
@@ -64,5 +125,12 @@ async function main() {
     process.exit(1);
   }
 }
+
+let myAdd: (x: number, y: number) => number = function (
+  x: number,
+  y: number
+): number {
+  return x + y;
+};
 
 main();
